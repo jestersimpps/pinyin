2b8cc62b9019ef49752e8234163d3236e14d0141
Jo Vinkenroye <jestersimpps@gmail.com>
2025-06-27 10:38:03 +0200

    disy

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/daisyui/dist/**/*.js',
   ],
   theme: {
     extend: {},
@@ -11,5 +12,6 @@ module.exports = {
   plugins: [require('daisyui')],
   daisyui: {
     themes: ["light", "dark"],
+    logs: false,
   },
 }
